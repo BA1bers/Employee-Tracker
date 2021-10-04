27431c567f1424257d0a352581e77e4d233bb708
BA1bers <brent.albers@yahoo.com>
2021-10-03 17:08:17 -0700

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,17 @@
 INSERT INTO Department (name)
 VALUES 
-    ("Sales"),
+    ("Animation"),
     ("Finance"),
     ("Legal"),
-    ("Engineering");
+    ("Writing");
 
 
 INSERT INTO Role (title, salary, department_id)
 VALUES 
-    ("Sales Lead", 80000, 1),
-    ("Sales Marketing", 65000, 1),
-    ("Client Services", 60000, 1),
-    ("Account Manager", 75000, 2),
+    ("Animation Lead", 80000, 1),
     ("Finance Analyst", 80000, 2),
-    ("Legal Team Lead", 100000, 3),
-    ("Contract Admin", 110000, 3),
     ("Lawyer", 95000, 3),
-    ("Lead Engineer", 110000, 4),
-    ("Software Engineer", 85000, 4),
-    ("Systems Engineer", 85000, 4);
+    ("Lead Writer", 70000, 4);
 
 
 INSERT INTO Employee (first_name, last_name, role_id, manager_id)
